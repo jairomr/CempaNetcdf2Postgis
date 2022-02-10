f6128d680ec79fad66cb29c9115336dd9bda36ce
Jairo Matos Da Rocha <nxgame2009@gmail.com>
2022-02-10 12:41:04 -0300

    black and isort

--- src/cempa/netCDFtoTIFF.py
+++ src/cempa/netCDFtoTIFF.py
@@ -1,16 +1,19 @@
-import geopandas as gpd
-import pandas as pd
-import numpy as np
-from matplotlib.tri import Triangulation, LinearTriInterpolator 
-from rasterio.transform import Affine
-from rasterio.crs import CRS
-import rasterio
-from cempa.config import settings, lats, lons, logger
-from cempa.functions import get_time
-from generatmap.map import creat_map_file
 import os
 from os.path import isdir, isfile
 from time import time
+
+import geopandas as gpd
+import numpy as np
+import pandas as pd
+import rasterio
+from matplotlib.tri import LinearTriInterpolator, Triangulation
+from rasterio.crs import CRS
+from rasterio.transform import Affine
+
+from cempa.config import lats, logger, lons, settings
+from cempa.functions import get_time
+from generatmap.map import creat_map_file
+
 
 def nc2tiff(name, var, coll_name, file_name):
     path_level1 = f"{settings.DIRMAP}/{get_time(file_name,True)}"
@@ -20,26 +23,33 @@ def nc2tiff(name, var, coll_name, file_name):
         os.mkdir(f"{path_level1}/{name}")
     name_tif = f"{path_level1}/{name}/{coll_name}.tif"
     name_map = f"{path_level1}/{name}/{coll_name}.map"
-    if isfile(name_tif) and isfile(f"{name_map}") and not settings.forceCreateFiles:
+    if (
+        isfile(name_tif)
+        and isfile(f"{name_map}")
+        and not settings.forceCreateFiles
+    ):
         logger.info("Tiff e map ja foi gerado")
         return None
     start_time = time()
-    logger.info(f"Criando tiff /{get_time(file_name,True)}/{name}/{coll_name}.tif")
+    logger.info(
+        f"Criando tiff /{get_time(file_name,True)}/{name}/{coll_name}.tif"
+    )
     vtime, latitudes_grid, longitudes_grid = [
         x.flatten()
         for x in np.meshgrid(get_time(file_name), lats, lons, indexing="ij")
     ]
     tmp_var = var
     data = {
-                "date_time": vtime,
-                "lat": latitudes_grid,
-                "lon": longitudes_grid,
-                f"{name}": tmp_var,
-            }
-    temp_df = pd.DataFrame(
-            data
-        )
-    min_max = (round(np.nanmin(tmp_var), 2), round(np.nanmax(tmp_var) + 0.05, 2))
+        "date_time": vtime,
+        "lat": latitudes_grid,
+        "lon": longitudes_grid,
+        f"{name}": tmp_var,
+    }
+    temp_df = pd.DataFrame(data)
+    min_max = (
+        round(np.nanmin(tmp_var), 2),
+        round(np.nanmax(tmp_var) + 0.05, 2),
+    )
     tgpdf = gpd.GeoDataFrame(
         temp_df, geometry=gpd.points_from_xy(temp_df.lon, temp_df.lat)
     )
@@ -63,7 +73,7 @@ def nc2tiff(name, var, coll_name, file_name):
     linTriFn = LinearTriInterpolator(triFn, totalPointsArray[:, 2])
 
     # define raster resolution in (m)
-    rasterRes = settings.rasterRes # 0.01
+    rasterRes = settings.rasterRes  # 0.01
 
     xCoords = np.arange(
         totalPointsArray[:, 0].min(),
@@ -90,9 +100,7 @@ def nc2tiff(name, var, coll_name, file_name):
     transform = Affine.translation(
         xCoords[0] - rasterRes / 2, yCoords[0] - rasterRes / 2
     ) * Affine.scale(rasterRes, rasterRes)
-    
-    
-    
+
     triInterpRaster = rasterio.open(
         name_tif,
         "w",
@@ -107,6 +115,6 @@ def nc2tiff(name, var, coll_name, file_name):
     )
     triInterpRaster.write(zCoords, 1)
     triInterpRaster.close()
-    creat_map_file(name_tif,  coll_name, min_max= min_max, geotiff = True)
+    creat_map_file(name_tif, coll_name, min_max=min_max, geotiff=True)
     logger.info(f"Tempo de crianção para tif e map: {time() - start_time}")
     return None
