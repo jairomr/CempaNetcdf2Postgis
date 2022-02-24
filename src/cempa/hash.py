258ee25d68473ba2446b8f2f9dc453fe0126ec85
Jairo Matos Da Rocha <nxgame2009@gmail.com>
2022-02-24 09:16:27 -0300

    config

--- src/cempa/hash.py
+++ src/cempa/hash.py
@@ -6,7 +6,7 @@ from pandas.util import hash_pandas_object
 
 def generate_file_md5(filename: str, blocksize=2 ** 20) -> str:
     m = md5()
-    with open(filename, "rb") as f:
+    with open(filename, 'rb') as f:
         while True:
             buf = f.read(blocksize)
             if not buf:
@@ -18,5 +18,5 @@ def generate_file_md5(filename: str, blocksize=2 ** 20) -> str:
 def data_frame2hash(name: str, df: DataFrame) -> str:
     m = md5()
     for i in hash_pandas_object(df):
-        m.update(str(i).encode("utf-8"))
-    return f"{name}_{m.hexdigest()}"
+        m.update(str(i).encode('utf-8'))
+    return f'{name}_{m.hexdigest()}'
