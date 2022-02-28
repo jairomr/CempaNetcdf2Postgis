dffde10b4dac95d0e5aa1cece37524660229736f
Jairo Matos Da Rocha <nxgame2009@gmail.com>
2022-02-28 00:43:39 -0300

    call back

--- src/cempa/db.py
+++ src/cempa/db.py
@@ -5,14 +5,8 @@ from sqlalchemy.orm import sessionmaker
 from cempa.config import settings
 
 Base = declarative_base()
-keepalive_kwargs = {
-    "keepalives": 1,
-    "keepalives_idle": 30,
-    "keepalives_interval": 5,
-    "keepalives_count": 5,
-}
 
-engine = create_engine(settings.dblink, pool_pre_ping=True, **keepalive_kwargs)
+engine = create_engine(settings.dblink, pool_pre_ping=True)
 
 def create_session():
     Session = sessionmaker(bind=engine)
